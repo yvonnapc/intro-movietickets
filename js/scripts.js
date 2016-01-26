fd0ae7ad90b506378618eb3564077632895fa05c
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-01-26 15:46:45 -0800

    Add prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,33 @@ function Ticket(age, film, day, time) {
   this.film = film;
   this.day = day;
   this.time = time;
+  this.basePrice = 15;
+}
+
+Ticket.prototype.findPrice =function() {
+
+  if((this.age >= 5) || (this.age <= 12))
+  { return (this.basePrice -= 4);
+  } else if (this.time < 20) {
+    return (this.basePrice -= 4);
+  } else {
+    return (this.basePrice);
+  }
 }
 
 $(document).ready(function() {
 
   $("form#new-ticket").submit(function(event){
     event.preventDefault();
+
     var inputtedAge = parseInt($("input#age").val());
     var inputtedFilm = $("input#film").val();
     var inputtedDay = $("input#day").val();
     var inputtedTime = parseInt($("input#time").val());
-
-    var newTicket =
-
+    var newTicket = new Ticket (inputtedAge, inputtedFilm, inputtedDay, inputtedTime);
+    var totalPrice = newTicket.findPrice();
+console.log(newTicket);
+  $("#print").text(totalPrice);
 
   });
 });
